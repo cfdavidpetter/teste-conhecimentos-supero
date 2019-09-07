09c0d9b29b89527efd6693448f2316eb88783be1
David Petter <cfdavidpetter@gmail.com>
2019-09-07 03:04:25 -0300

    Criado Client.

---
+++ pergunta-quatro/client/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="app">
+        <section class="hero is-primary is-mobile is-fullheight">
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <div class="row">
+                        <div class="columns">
+                            <div class="column is-half is-offset-one-quarter">
+                                <tarefa-card></tarefa-card>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+import TarefaCard from './components/TarefaCard'
+
+export default {
+    name: 'app',
+    components: {
+        TarefaCard
+    },
+    data () {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
+    }
+}
+</script>
+
+<style>
+</style>
