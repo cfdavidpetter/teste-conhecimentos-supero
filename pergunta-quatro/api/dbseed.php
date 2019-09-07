3d56ed46f690c70c05fed326ccfd9cc20972942a
David Petter <cfdavidpetter@gmail.com>
2019-09-07 17:31:42 -0300

    Criando script para o dbseed.php

--- pergunta-quatro/api/dbseed.php
+++ pergunta-quatro/api/dbseed.php
@@ -1,7 +1,24 @@
 <?php
 require 'bootstrap.php';
 
-$statement = '';
+$statement = "
+    CREATE TABLE tarefas (
+        id INT(11) NOT NULL AUTO_INCREMENT,
+        titulo VARCHAR(100) NOT NULL,
+        descricao TEXT NULL,
+        prioridade INT(1) NOT NULL DEFAULT '1',
+        PRIMARY KEY (`id`)
+    );
+
+    INSERT INTO tarefas (titulo, descricao, prioridade) VALUES ('Phasellus feugiat', 'Phasellus feugiat mi sed sodales iaculis. Nunc massa erat, aliquet scelerisque varius eget, mattis ac justo.', 1);
+    INSERT INTO tarefas (titulo, descricao, prioridade) VALUES ('Viverra', 'Nam aliquet velit nisi, sed blandit justo interdum a.', 1);
+    INSERT INTO tarefas (titulo, descricao, prioridade) VALUES ('Dignissim', 'Pellentesque pretium semper finibus.', 2);
+    INSERT INTO tarefas (titulo, descricao, prioridade) VALUES ('Phasellus feugiat', 'Pellentesque vitae odio sed quam sagittis aliquam et a neque.', 3);
+    INSERT INTO tarefas (titulo, descricao, prioridade) VALUES ('Etiam sem mi', 'Nulla quis finibus magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.', 3);
+    INSERT INTO tarefas (titulo, descricao, prioridade) VALUES ('Phasellus feugiat', 'Pellentesque pretium semper finibus.', 1);
+    INSERT INTO tarefas (titulo, descricao, prioridade) VALUES ('Nullam vitae venenatis', 'Phasellus feugiat mi sed sodales iaculis. Nunc massa erat, aliquet scelerisque varius eget, mattis ac justo.', 3);
+    INSERT INTO tarefas (titulo, descricao, prioridade) VALUES ('Sristique senectus et netus', 'Sed dignissim quis felis nec bibendum.', 2);
+";
 
 try {
     $createTable = $dbConnection->exec($statement);
